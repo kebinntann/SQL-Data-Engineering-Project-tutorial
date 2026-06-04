60c3b9a2ecee7aaa4145a4d49a6bd2306b92ad0f
Kevin Tan <kev.tan2346@gmail.com>
2026-06-05 00:29:31 +0800

    load schemas & update master build script

--- 2_DW_mart/build_dw_marts.sql
+++ 2_DW_mart/build_dw_marts.sql
@@ -6,10 +6,10 @@
 
 
 -- Step 1: DW - Create star schema tables
-.read
+.read 01_create_tables_dw.sql
 
 -- Step 2: DW - Load data from CSV files into star schema
-.read
+.read 02_load_schema_dw.sql
 
 -- Step 3: Mart - Create flat mart (denormalized table)
 .read
